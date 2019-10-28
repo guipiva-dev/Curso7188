af3fa7120f4b971119fe9f6ce7b282108835298c
gpiva <guilhermepiva@hotmail.com>
2019-10-28 11:55:48 -0300

    Aula 8 - Tela de Login Parte 2

--- src/app/pages/account/login/login.page.ts
+++ src/app/pages/account/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoadingController, NavController } from '@ionic/angular';
 
 import { DataService } from './../../../services/data.service';
@@ -11,14 +11,34 @@ import { DataService } from './../../../services/data.service';
 })
 export class LoginPage implements OnInit {
 
+form: FormGroup;
+hide: boolean = true;
+
   constructor(
     private fb: FormBuilder,
     private LoadingCtrl: LoadingController,
     private navCtrl: NavController,
     private dataService: DataService
-  ) { }
+  ) { 
+    this.form = fb.group({
+      username: ['',
+      Validators.compose([
+        Validators.required
+      ])],
+      password: ['',
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ])]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  toggleHide() {
+    this.hide = !this.hide;
   }
 
 }
