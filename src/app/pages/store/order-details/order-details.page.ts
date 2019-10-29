e3f7de5663ff3af077be7cfd71e379526e0cb1c4
gpiva <guilhermepiva@hotmail.com>
2019-10-29 17:55:37 -0300

    Aula 23 - Order Details

--- src/app/pages/store/order-details/order-details.page.ts
+++ src/app/pages/store/order-details/order-details.page.ts
@@ -1,4 +1,10 @@
+import { StatusUtils } from './../../../utils/status.utils';
+import { SecurityUtils } from './../../../utils/security.utils';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { OrderModel } from 'src/app/models/order.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-order-details',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailsPage implements OnInit {
 
-  constructor() { }
+  order: OrderModel;
+
+  constructor(private dataService: DataService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let number = this.route.snapshot.paramMap.get('number');
+    this.dataService.getOrder(number)
+      .subscribe(
+        (res) => {
+          this.order = res;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
+  isManager(): boolean {
+    return SecurityUtils.isInRole('manager');
+  }
+
+  translatedOrderStatus(status: string): string {
+    return StatusUtils.convert(status);
+  }
 }
